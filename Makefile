ff6e55d06dab25c82de47324673a2aaa0d8ebe65
celie56 <celie56@gmail.com>
2015-03-29 17:47:28 -0400

    updated Makefile to output to folder named output

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 
 all:
-	cp typescript-test/index.html ./
+	#martina's stuff
+	cp  pageLoadTest/* ./output/
 	cp typescript-test/js/blackjack.js ./js/
---
+++ output/main.js
@@ -0,0 +1,7 @@
+$(function(){
+	$(".button_link").click(function(){
+		$loc = $(this).attr("id") + ".html";
+		$("#content").load($loc);
+		window.alert("hola");
+	});
+});
