e187c4893f043f6c7ea096120da2f92ebac27977
martinalu <elisalu@umich.edu>
2015-04-04 02:03:07 -0400

    options

--- pageLoadTest/main.js
+++ pageLoadTest/main.js
@@ -2,6 +2,21 @@ $(function(){
 	$(".button_link").click(function(){
 		$loc = $(this).attr("id") + ".html";
 		$("#content").load($loc);
-		window.alert("hola");
 	});
 });
+
+
+
+
+var cheats = function() {
+	var x = document.getElementById("myonoffswitch").checked;
+	if(x == true)
+	{
+		//cheats on 
+	}	
+	else
+	{
+		//cheats off 
+	}	
+    
+}
