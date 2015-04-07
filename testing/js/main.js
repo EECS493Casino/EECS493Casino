31ea72c81eb71e0c63d26bb5362fa04ebed57e52
Troy <tpdietz@umich.edu>
2015-04-07 14:29:15 -0400

    First check in of buggy betting Also a failed attempt at mergin game with current menu/background design

---
+++ testing/js/main.js
@@ -0,0 +1,41 @@
+
+$(document).ready(function(){
+	if($(".button_link")) {
+		$(".button_link").click(function(){
+			$loc = $(this).attr("id") + ".html";
+			//window.alert($loc);
+			$("#content").load($loc);
+		});
+	}
+});
+
+
+
+
+var cheats = function() {
+	var x = document.getElementById("myonoffswitch").checked;
+	if(x == true)
+	{
+		//cheats on 
+		window.alert("cheats on");
+	}	
+	else
+	{
+		//cheats off 
+	}	
+    
+}
+
+var level = function(input){
+
+	var lvl = input.value;
+	
+	if(input.value == "hard")
+	{
+		window.alert(input.value);
+	}	
+	else{
+
+	}
+
+}
