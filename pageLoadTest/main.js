26ca5c7b90af3cfb026d5d111bfbdb9e86aeed77
martinalu <elisalu@umich.edu>
2015-04-13 19:17:42 -0400

    tables are back

--- pageLoadTest/main.js
+++ pageLoadTest/main.js
@@ -25,14 +25,33 @@ var cheats = function() {
 
 var level = function(input){
 
-	var lvl = input.value;
-	
-	if(input.value == "hard")
-	{
-		// window.alert(input.value);
-	}	
-	else{
 
-	}
+	var x = document.getElementById("levelswitch").checked;
+	if(x == true)
+	{
+		//easy
+		//window.alert("easy");
+	}	
+	else
+	{
+		//difficult
+	}	
+
+}
+
+var music = function(input){
+
+
+	var x = document.getElementById("musicswitch").checked;
+	if(x == true)
+	{
+		//music on
+		window.alert("Music feature not implemented yet.");
+	}	
+	else
+	{
+		//music off
+		window.alert("Music feature not implemented yet.");
+	}	
 
 }
